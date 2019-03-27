9f1e013de7e933fd4c541745416166163a2bb818
forma1710 <forma1710@tftic.be>
2019-03-27 13:37:07 +0100

    start switch

--- src/be/tftic/Main.java
+++ src/be/tftic/Main.java
@@ -1,8 +1,21 @@
 package be.tftic;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	System.out.println("coucou Thierry");
+	// Choix de l'utilisateur
+	String userInput;
+	// scan pour les strings
+	Scanner stringScanner=new Scanner(System.in);
+	//scan pour le reste
+	Scanner intScanner=new Scanner(System.in);
+	while (!userInput.equals("stop")){
+	    userInput=stringScanner.nextLine();
+	    switch (userInput):
+        case "add":
+
+        }
     }
 }
