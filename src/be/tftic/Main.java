402e018e9688275e7d21838289a6cea3e399fdf2
restproof40 <restproof@gmail.com>
2019-03-27 14:31:13 +0100

    Added - Service Repository[WIP]

--- src/be/tftic/Main.java
+++ src/be/tftic/Main.java
@@ -1,10 +1,11 @@
 package be.tftic;
 
 import be.tftic.models.Blague;
+import be.tftic.service.Repository;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        
     }
 }
